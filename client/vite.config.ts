a5322eff90ff606310e6fd1f04c6ddca20b9f522
Simon Bond <sbond@simonbond.uk>
2026-06-09 19:00:45 +0100

    Fix PWA API caching

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,4 +1,11 @@
-VitePWA({
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
   registerType: "autoUpdate",
   includeAssets: ["favicon.svg"],
   workbox: {
@@ -42,3 +49,5 @@ VitePWA({
     ]
   }
 })
+  ]
+});
